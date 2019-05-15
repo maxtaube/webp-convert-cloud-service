2210a3cc143e3c8a6d3ddb7e11c23e6d930685c1
Maximilian Taube <max.taube@hotmail.com>
2019-05-15 12:28:41 +0200

    Update script.php

--- script.php
+++ script.php
@@ -1,3 +1,24 @@
+<?php require 'vendor/autoload.php';
+
+use \WebPConvertCloudService\WebPConvertCloudService;
+
+$options = [
+    // Set dir for storing converted images temporarily
+    // (make sure to create that dir, with permissions for web server to write)
+    'destination-dir' => '../conversions',
+
+    // Set acccess restrictions
+    'access' => [
+        'whitelist' => [
+            [
+                'ip' => '*',
+                'api-key' => 'my dog is white',
+                'require-api-key-to-be-crypted-in-transfer' => false
+            ]
+        ]
+    ],
+
+    // Optionally set webp-convert options
     'webp-convert' => [
         'converters' => ['cwebp', 'gd', 'imagick'],
         'converter-options' => [
@@ -11,3 +32,5 @@
 ];
 
 $wpc = new WebPConvertCloudService();
+$wpc->handleRequest($options);
+?>
