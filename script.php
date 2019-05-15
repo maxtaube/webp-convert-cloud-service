3a6e15e432fae5aed1ab810a11534e787fc8f58d
Maximilian Taube <max.taube@hotmail.com>
2019-05-15 11:35:15 +0200

    asdad

--- script.php
+++ script.php
@@ -1,24 +1,3 @@
-require 'vendor/autoload.php';
-
-use \WebPConvertCloudService\WebPConvertCloudService;
-
-$options = [
-    // Set dir for storing converted images temporarily
-    // (make sure to create that dir, with permissions for web server to write)
-    'destination-dir' => '../conversions',
-
-    // Set acccess restrictions
-    'access' => [
-        'whitelist' => [
-            [
-                'ip' => '*',
-                'api-key' => 'abcdefghijklmnop',
-                'require-api-key-to-be-crypted-in-transfer' => false
-            ]
-        ]
-    ],
-
-    // Optionally set webp-convert options
     'webp-convert' => [
         'converters' => ['cwebp', 'gd', 'imagick'],
         'converter-options' => [
@@ -32,4 +11,3 @@ $options = [
 ];
 
 $wpc = new WebPConvertCloudService();
-$wpc->handleRequest($options);
