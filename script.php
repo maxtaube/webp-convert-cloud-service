fea6761c91b10aa3783b2a1bd44dbd79260c81a6
maxtaube <max.taube@hotmail.com>
2019-05-15 23:11:59 +0200

    sdasda

--- script.php
+++ script.php
@@ -17,6 +17,18 @@ $options = [
             ]
         ]
     ],
+    
+    // Optionally set webp-convert options
+    'webp-convert' => [
+        'converters' => ['cwebp', 'gd', 'imagick'],
+        'converter-options' => [
+            'cwebp' => [
+                'try-common-system-paths' => true,
+                'try-supplied-binary-for-os' => true,
+                'use-nice' => true
+            ]
+        ]
+    ]
 
     
 ];
