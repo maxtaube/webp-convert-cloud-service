01a2dbb3c47d8fc6b6e4d755cdd3cadb1edab0c9
Maximilian Taube <max.taube@hotmail.com>
2019-05-15 22:58:55 +0200

    rework (vendor back)

--- script.php
+++ script.php
@@ -18,17 +18,7 @@ $options = [
         ]
     ],
 
-    // Optionally set webp-convert options
-    'webp-convert' => [
-        'converters' => ['cwebp', 'gd', 'imagick'],
-        'converter-options' => [
-            'cwebp' => [
-                'try-common-system-paths' => true,
-                'try-supplied-binary-for-os' => true,
-                'use-nice' => true
-            ]
-        ]
-    ]
+    
 ];
 
 $wpc = new WebPConvertCloudService();
